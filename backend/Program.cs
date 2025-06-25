031b6ca02cd78af8711be09ce75b145e1207d103
orisimh <zori1232@gmail.com>
2025-06-26 02:27:28 +0300

    commit

--- backend/Program.cs
+++ backend/Program.cs
@@ -34,7 +34,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
